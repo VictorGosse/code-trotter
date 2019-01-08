7877979ad949fb0e7bccf3e84e9ce61d5f62cd5b
Victor Gosse <gosse.web@gmail.com>
2019-01-08 08:31:44 +0100

    Fix noopener + remove manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,17 +63,6 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-      },
-    },
-    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: 'jw3pwll4r1ki',
