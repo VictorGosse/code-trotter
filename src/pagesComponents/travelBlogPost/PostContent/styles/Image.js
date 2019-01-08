d430831d7fa3f26b5172b41cc076853ba116206f
Victor Gosse <gosse.web@gmail.com>
2019-01-08 09:07:19 +0100

    Fix DOM on travel posts

--- src/pagesComponents/travelBlogPost/PostContent/styles/Image.js
+++ src/pagesComponents/travelBlogPost/PostContent/styles/Image.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components'
 
 const Image = styled('img')`
-  width: 100%;
+  width: 120%;
+  margin-left: -10%;
+  margin-top: 25px;
+  height: auto;
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 100%;
+    margin-left: 0;
+  }
 `
 
 export default Image
--- src/pagesComponents/travelBlogPost/PostContent/styles/ImageLegend.js
+++ src/pagesComponents/travelBlogPost/PostContent/styles/ImageLegend.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
-const ImageLegend = styled('figcaption')`
+const ImageLegend = styled('p')`
   text-align: center;
   text-transform: uppercase;
   font-size: ${({ theme }) => theme.fonts.fontSize.fs12};
   font-weight: ${({ theme }) => theme.fonts.fontWeight.fw500};
   line-height: ${({ theme }) => theme.fonts.lineHeight.lh14};
   color: ${({ theme }) => theme.colors.silver};
+  margin-bottom: 25px;
 `
 
 export default ImageLegend
