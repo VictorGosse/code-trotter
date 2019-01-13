a9db491e80a7c04730f92dcfae0d7ddfbfec7221
Victor Gosse <gosse.web@gmail.com>
2019-01-13 18:01:28 +0100

    Fix image size travels

--- src/pagesComponents/travelBlogPost/PostContent/styles/Image.js
+++ src/pagesComponents/travelBlogPost/PostContent/styles/Image.js
@@ -5,6 +5,8 @@ const Image = styled('img')`
   margin-left: -10%;
   margin-top: 25px;
   height: auto;
+  max-height: 600px;
+  object-fit: cover;
   @media only screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 100%;
     margin-left: 0;
