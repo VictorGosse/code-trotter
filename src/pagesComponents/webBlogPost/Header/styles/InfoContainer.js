791c9eadc8c90e13333c26ac4ab91ad03fff64f1
Victor Gosse <gosse.web@gmail.com>
2019-02-01 09:04:18 +0100

    push

--- src/pagesComponents/webBlogPost/Header/styles/InfoContainer.js
+++ src/pagesComponents/webBlogPost/Header/styles/InfoContainer.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const InfoContainer = styled('div')`
   width: ${({ theme }) => theme.webBlogPost.basicWidth};
-  margin: 35px auto 25px auto;
+  margin: 35px auto 25px auto; 
   @media only screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 100%;
   }
