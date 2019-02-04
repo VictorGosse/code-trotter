c5769e6d42237ae500503622b3600f82f583edb6
Victor Gosse <gosse.web@gmail.com>
2019-02-04 10:25:06 +0100

    Target blank on links on web posts

--- src/pagesComponents/webBlogPost/PostContent/index.js
+++ src/pagesComponents/webBlogPost/PostContent/index.js
@@ -35,7 +35,7 @@ const Header3 = styled(AttiHeader3)`
 const Image = styled(PostImage)`
 `
 
-const Link = styled(AttiLink)`
+const Link = styled(AttiLink).attrs({ target: '_blank' })`
   font-size: ${({ theme }) => theme.fonts.fontSize.fs16};
   line-height: 28px;
 `
