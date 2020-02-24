9f610c1797545766c591a8c92851ef48e2942f16
Victor Gosse <vgosse@mailjet.com>
2020-02-24 07:53:29 +0100

    Add GSC code

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,10 @@ const Index = () => {
         description="Vous pouvez retrouver ici des articles de mes précédents voyages et des articles techniques liés au web, au développement, à la gestion de projet..."
         keywords={["voyages", "web", "developpement"]}
         title="Les voyages et le web, mes 2 passions"
+        extraMeta={[{
+          name: 'google-site-verification',
+          content: '1E806lu_l9hGS22xEiRG5Y-4iXNHwJqPYEUfwbbWixw',
+        }]}
       />
       <Categories />
     </>
